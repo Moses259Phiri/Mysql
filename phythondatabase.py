91af0eda0f3b2ed773398173071783d78d519b98
Lehlohonolo Mopeli <71072285+LehlohonoloMopeli@users.noreply.github.com>
2021-12-20 11:53:28 +0200

    Added commands to setup environment

---
+++ phythondatabase.py
@@ -0,0 +1,7 @@
+"""
+    Create Virtual environment $ python -m venv env
+    Activate virtual environment $ source ".\env\Scripts\activate"
+    Install python packages $ pip install -r requirements.txt
+    Update packages $ pip freeze > requirements.txt
+    
+"""
