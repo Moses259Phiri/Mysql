2839726e36b313edbc21cbae6705edfb0f8ca66f
Lehlohonolo Mopeli <71072285+LehlohonoloMopeli@users.noreply.github.com>
2021-12-20 12:03:02 +0200

    Added database connection

--- phythondatabase.py
+++ phythondatabase.py
@@ -3,5 +3,12 @@
     Activate virtual environment $ source ".\env\Scripts\activate"
     Install python packages $ pip install -r requirements.txt
     Update packages $ pip freeze > requirements.txt
-    
+
 """
+
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root"
+)
